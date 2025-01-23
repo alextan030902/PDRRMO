4a847361a4a719c8dcaa0c241ce94faf6389c854
RalphADF <146459628+RalphADF@users.noreply.github.com>
2025-01-23 15:34:03 +0800

    aboutpdrrmo

--- resources/views/about-pdrrmo/index.blade.php
+++ resources/views/about-pdrrmo/index.blade.php
@@ -2,36 +2,121 @@
 
 @section('content')
 
-<div class="container my-5">
-    <div class="row">
-        <!-- First Card -->
-        <div class="col-lg-6 mb-4">
-            <div class="card">
-                <img src="https://via.placeholder.com/800x400" class="card-img-top" alt="Card Image 1">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title 1</h5>
-                    <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to additional content. 
-                        This content is a little bit longer.
-                    </p>
-                    <a href="#" class="btn btn-primary">Learn More</a>
-                </div>
-            </div>
+<div class="container my-5 ">
+    <div class="row border border-black rounded" style="margin-bottom: 40px; ">
+        <h2 class="card-title text-center " style="margin-bottom: 40px; color:#FE6305; "><strong>About PDRRMO</strong>
+        </h2>
+        <p class="card-text">
+            Today, Bulacan Rescue has a total of 60 staff, with 19 permanent, 39 casual and 2 job order appointment
+            status. And also 2 police officers from Bulacan Police Provincial Office (BPPO) and 2 fire officers from
+            Office of the Provincial Fire Marshall (OPFM) are currently detailed at Bulacan Rescue to assist in
+            responding on emergencies.
+        </p>
+
+        <p class="card-text">
+            Geared towards the improvement of service, it created the Bulacan Public Safety Solution Project, that
+            facilitates the installation of hydrological monitoring tools and 30 CCTVs in major road intersections along
+            the Mc Arthur highway to be able to respond promptly and timely. Bulacan Rescue take advantage of
+            the state-of-the-art facilities that would provide them with early warning in doing disaster response.,
+            thereby achieving the objective of a faster and more efficient service provided to all. This includes early
+            warning provided via CCTV and hydrological monitoring tool in the River Basins of Pampanga, Santa Maria and
+            Angat.
+        </p>
+        <p class="card-text" style="margin-bottom: 40px; ">
+            Last October 28, 2021 was the Opening Day for Bulacan 911 Project.  Executive Order (EO) No. 56 (s. 2018)
+            institutionalized the emergency 911 hotline as the  nationwide  emergency answering point that conform with
+            international standards on emergency numbers for public telecommunication networks.  In  911-Bulacan, the
+            general public  only have to remember three digits, in order to call and ask for help in cases that need
+            immediate attention. Being able to call 911 in a swift dial via cellphones or other communicative
+            device will make a difference of minutes or seconds which shall have affect you or someone else’s health and
+            safety during the emergency itself. The 911 emergency hotline calls can be an avenue to streamline all
+            classifications of emergency in order to tap the agency which has the expertise to address a certain kind of
+            emergency. The 911 calls can dispatch either ambulances, fire, the EMS, WASAR team, etc.
+        </p>
+    </div>
+    <div class="row" style="margin-bottom: 40px;">
+    <div class="col-md-8">
+        <h2 class="card-title  mb-3" style="color:#FE6305;">VISION</h2>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla.
+            </p>
         </div>
-        <!-- Second Card -->
-        <div class="col-lg-6 mb-4">
-            <div class="card">
-                <img src="https://via.placeholder.com/800x400" class="card-img-top" alt="Card Image 2">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title 2</h5>
-                    <p class="card-text">
-                        This is another card with supporting text below as a natural lead-in to additional content. 
-                        It contains some extra text to balance the layout.
-                    </p>
-                    <a href="#" class="btn btn-primary">Learn More</a>
-                </div>
-            </div>
+        <div class="col-md-4 d-flex justify-content-between">
+            <!-- First Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 1" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Second Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 2" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Third Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 3" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
         </div>
     </div>
-</div>
+
+    <div class="row" style="margin-bottom: 40px;">
+    <div class="col-md-8">
+        <h2 class="card-title  mb-3" style="color:#FE6305;">MISSION</h2>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla.
+            </p>
+        </div>
+        <div class="col-md-4 d-flex justify-content-between">
+            <!-- First Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 1" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Second Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 2" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Third Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 3" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+        </div>
+    </div>
+
+
+    <div class="row" style="margin-bottom: 40px;">
+    <div class="col-md-8">
+        <h2 class="card-title  mb-3" style="color:#FE6305;">Functions, Duties and Responsibilities</h2>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat
+                nulla.
+            </p>
+        </div>
+        <div class="col-md-4 d-flex justify-content-between">
+            <!-- First Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 1" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Second Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 2" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+            <!-- Third Image -->
+            <img src="https://c8.alamy.com/comp/2H0FHX7/flood-abstract-concept-vector-illustration-2H0FHX7.jpg"
+                alt="Image 3" class="img-fluid" style="max-width: 100px; height: 100px; border-radius: 50%;">
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <h2 class="card-title text-center " style="margin-top: 40px; margin-bottom: 40px; color:#FE6305; "><strong>Organizational Structure</strong>
+        </h2>
+    </div>
+    <div class="row">
+          <div class="col-12 text-center">
+             <img src="{{ asset('assets/img/OrgStruct.png') }}" alt="Banner Image" class="img-fluid banner-image">
+          </div>
+      </div>
 @endsection
